a0ac29be16f02a6d66aa7c3310c85bc355dfb899
VentusFair <ventus.fair@gmail.com>
2018-02-07 14:53:46 +0200

    lisattiin luokka

---
+++ src/main/java/com/example/Bookstore/Controller/MyController.java
@@ -0,0 +1,17 @@
+package com.example.Bookstore.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class MyController {	
+	
+	@RequestMapping("/index")
+	public String home() {
+		// pääsivu kirjakaupalle
+		return "index";
+	}
+	
+	
+}
