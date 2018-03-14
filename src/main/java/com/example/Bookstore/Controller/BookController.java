a23717f7c295525e821e83066861b5a81eeb5401
VentusFair <ventus.fair@gmail.com>
2018-03-14 11:04:04 +0200

    Tehtava nelja

--- src/main/java/com/example/Bookstore/Controller/BookController.java
+++ src/main/java/com/example/Bookstore/Controller/BookController.java
@@ -1,14 +1,18 @@
 package com.example.Bookstore.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Bookstore.Books.Book;
 import com.example.Bookstore.Books.BookRepository;
+
 
 
 
@@ -34,6 +38,12 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
+	
+	// RESTful service to get all books
+    @RequestMapping(value="/api", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }   
   
     //Add new book
     @RequestMapping(value = "/add")
@@ -41,6 +51,12 @@ public class BookController {
     	model.addAttribute("book", new Book());
         return "addbook";
     }
+    
+ // RESTful service to get book by id
+    @RequestMapping(value="/api/{id}", method = RequestMethod.GET)
+    public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findOne(bookId);
+    } 
     
     // Save new book
     @RequestMapping(value = "/save", method = RequestMethod.POST)
